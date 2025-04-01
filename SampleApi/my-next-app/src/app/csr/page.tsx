58f2e0a6aee11bf66f813d7a7267024422366104
Carles <carles.davila@gmail.com>
2025-04-01 20:45:45 +0200

    sample added

---
+++ SampleApi/my-next-app/src/app/csr/page.tsx
@@ -0,0 +1,33 @@
+﻿"use client"; // 👈 Esto es obligatorio en Next.js 13+ en el App Router
+
+import { useState, useEffect } from "react";
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+
+export default function CSRPage() {
+    const [post, setPost] = useState<Post | null>(null);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts/3")
+            .then((res) => res.json())
+            .then((data: Post) => setPost(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>CSR: Client-Side Rendering</h1>
+            {post ? (
+                <>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </>
+            ) : (
+                <p>Cargando...</p>
+            )}
+        </div>
+    );
+}
